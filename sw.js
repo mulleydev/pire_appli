ab0167fb1f946850841fdb538fadce07f441fc0c
Callan <cerveauxallanjean@gmail.com>
2019-10-16 12:48:09 +0200

    Initialazation PWA

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = "Worst PWA !";
+
+const fileToCache = [
+	'/',
+	'/index.html',
+	'/src',
+	'/dist'
+];
+
+self.addEventListener('install', (e) => {
+	console.log('[ServiceWorker] Install');
+	e.waitUntil(
+		caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(fileToCache);
+		})
+  );
+});
+
+self.addEventListener('fetch', event => {
+	event.responseWith(
+    caches.match(event.resquest, {ignoreSearch: true}).then(res => {
+    	return res || fetch(event.request);
+    }));
+});
